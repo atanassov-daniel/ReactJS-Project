1c4b6e55f8ea653ce7ef1665e67f528b0c5c56f4
atanassov-daniel <59175862+atanassov-daniel@users.noreply.github.com>
2021-04-25 23:22:06 +0300

    load user's teams

--- src/components/MyTeamsLogin/MyTeamsLogin.js
+++ src/components/MyTeamsLogin/MyTeamsLogin.js
@@ -1,56 +1,96 @@
 import { Component } from 'react';
-import { Input, Image, Typography, Row, Col, Button, Card } from 'antd';
+import { Input, Image, Typography, Row, Col, Button, Card, Avatar } from 'antd';
+import {ArrowRightOutlined} from '@ant-design/icons'
 
+import getMyTeams from '../../services/getMyTeams';
 import styles from './MyTeamsLogin.module.css';
 
 const { Link, Title } = Typography;
 
 class MyTeamsLogin extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            myTeams: null
+        };
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.myTeams);
+        if (this.state.myTeams === null) {
+            getMyTeams(this.props.authInfo.email)
+                // .then(teams => this.setState(() => ({ myTeams: teams })));
+                .then(teams => this.setState(() => ({ myTeams: teams })));
+        }
+    }
+
     render() {
+        /* console.log(this.props.authInfo.email);
+        const myTeams = getMyTeams(this.props.authInfo.email);
+        console.log(myTeams); */
+        /* const authInfo = this.props.authInfo;
+        let loading = !authInfo.isAuthenticated; // if not authenticated, then the status of loading will be true
+        if (!loading) {
+            loading = true;
+            getMyTeams(authInfo.email)
+                .then(teams => this.setState(() => ({ myTeams: teams })));
+        } */
+        /* getMyTeams(this.props.authInfo.email)
+            .then(teams => this.setState(() => ({ myTeams: teams }))); */
+
+        /* let myTeams = null;
+        getMyTeams(this.props.authInfo.email)
+            .then(teams => myTeams = teams); */
+
         return (
             <>
-                <div className={styles.contentWrapper}>
-                    <Row style={{ height: '100%', width: '100%', marginTop: '10%' }}>
-                        <Link href="/" style={{ margin: 'auto' }} >
-                            <Image style={{ width: 'auto' }} src="https://a.slack-edge.com/bv1-9/slack_logo-ebd02d1.svg" preview={false} height={34} />
-                        </Link>
-                    </Row>
+                {this.state.myTeams === null ?
+                    <div style={{ textAlign: 'center', fontSize: '10em', fontWeight: 'bold', fontWeight: 'bold' }}>STILL LOADING</div>
+                    :
+                    <div className={styles.contentWrapper}>
+                        <Row style={{ height: '100%', width: '100%', marginTop: '10%' }}>
+                            <Link href="/" style={{ margin: 'auto' }} >
+                                <Image style={{ width: 'auto' }} src="https://a.slack-edge.com/bv1-9/slack_logo-ebd02d1.svg" preview={false} height={34} />
+                            </Link>
+                        </Row>
 
-                    <Title level={2} style={{ textAlign: 'center', marginTop: '5%', fontWeight: 'bold', marginBottom: 'auto' }}><span style={{ color: '#480048' }}>Welcome back! </span>You look nice today.</Title>
-                    <p style={{ textAlign: 'center', color: 'dimgray', marginBottom: '3%' }}>Choose a workspace below to get back to working with your team.</p>
+                        <Title level={2} style={{ textAlign: 'center', marginTop: '5%', fontWeight: 'bold', marginBottom: 'auto' }}><span style={{ color: '#480048' }}>Welcome back! </span>You look nice today.</Title>
+                        <p style={{ textAlign: 'center', color: 'dimgray', marginBottom: '3%' }}>Choose a workspace below to get back to working with your team.</p>
 
 
-
-                    <Card
-                        // title="Workspaces for atanassov.daniel@gmail.com"
-                        title={
-                            <p style={{ fontWeight: 'normal', fontSize: '14.5px', margin: 0 }}>Workspaces for
+                        <Card
+                            // title="Workspaces for atanassov.daniel@gmail.com"
+                            title={
+                                <p style={{ fontWeight: 'normal', fontSize: '14.5px', margin: 0 }}>Workspaces for
                                 <span style={{ fontWeight: 'bold' }}> atanassov.daniel@gmail.com</span>
-                            </p>
-                        }
-                        bodyStyle={{ padding: '0.15em 0 0 1.5em' }}
-                    >
-                        <Card
-                            type="inner"
-                            className={styles.innerCard}
-                        // title="Inner Card title"
-                        // extra={<a href="#">More</a>}
+                                </p>
+                            }
+                            bodyStyle={{ padding: '0.15em 0 0 1.5em' }}
                         >
-                            Inner Card content
+                            {this.state.myTeams.map(team => (
+                                <Card
+                                    key={team?.name}
+                                    type="inner"
+                                    className={styles.innerCard}
+                                // title="Inner Card title"
+                                // extra={<a href="#">More</a>}
+                                >
+                                    <Card.Meta
+                                        avatar={
+                                            <Avatar src={team?.teamPicture || 'https://t4.ftcdn.net/jpg/02/51/95/53/240_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg'} shape="square" size={50} alt="team profile image" />
+                                        }
+                                        title={team?.name}
+                                        //TODO   responsive title ->     https://css-tricks.com/forums/topic/show-truncated-text-by-hovering-only-on-ellipsis/
+                                    // description={message.createdAt}
+                                    />
+                                    <Button style={{float: 'right'}}><ArrowRightOutlined /></Button>
+                                </Card>
+                            ))}
                         </Card>
-                        <Card
-                            // style={{ marginTop: 16 }}
-                            type="inner"
-                            className={styles.innerCard}
-                        // title="Inner Card title"
-                        // extra={<a href="#">More</a>}
-                        >
-                            Inner Card content
-                        </Card>
-                    </Card>
 
-
-                </div>
+                    </div>
+                }
             </>
         );
     }
---
+++ src/services/getMyTeams.js
@@ -0,0 +1,18 @@
+import { db } from '../utils/firebase';
+
+export default function getMyTeams(userEmail) {
+    return db.collection('users').doc(userEmail)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                doc = doc.data();
+                // const teams = doc.teams.sort((a,b) => {})
+
+                return doc.teams;
+            }
+
+            return null;
+        });
+
+        //!! get first 5 members from the actual tema, or have a cloud function that will update a 'membersOverview' field on the team in the user collection -> 2000 members will need to get an updated overview every time a user leaves probably; otherwise every user when logging in will have to get using .limit(5) the first memebers of every team he has ever joined
+};
